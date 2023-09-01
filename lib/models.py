5e366a684c95f53cb1eb2e890159cb13f2735e5e
kipngenohaaron <kipngenohaaron@gmail.com>
2023-09-01 18:55:32 +0300

    assignment done

--- lib/models.py
+++ lib/models.py
@@ -15,7 +15,6 @@ from sqlalchemy.ext.declarative import declarative_base
 engine = create_engine('sqlite:///migrations_test.db')
 
 Base = declarative_base()
-
 class Student(Base):
     __tablename__ = 'students'
 
